4afd4dade4c655195bd6cb5b68bc9edbd4127a8b
bilyzhang <cfn.zzq@hotmail.com>
2014-12-15 20:23:04 +0800

    第一次

---
+++ src/com/gamegift/ui/MSGActivity.java
@@ -0,0 +1,17 @@
+package com.gamegift.ui;
+
+import com.gamegift.ui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MSGActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.mes);
+	}
+
+}
